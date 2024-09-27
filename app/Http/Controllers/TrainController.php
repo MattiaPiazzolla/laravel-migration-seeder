7a8da2f7dad68061d7994fce686bd238cafb72c9
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-09-27 10:19:44 +0200

    [UPD] fixed repetition

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -8,10 +8,11 @@ use Illuminate\Http\Request;
 class TrainController extends Controller
 {
     public function index()
-{
-    $trains = Train::where('orario_di_partenza', '>=', now()->format('H:i:s'))->get();
-    return view('home', compact('trains'));
-}
+    {
+       
+        $trains = Train::all();
+        return view('home', compact('trains'));
+    }
 
 public function orari()
 {
